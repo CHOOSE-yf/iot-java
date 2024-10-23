7c7eceaa59cd0bc25ef6d3146f6249754d05e2a3
lixueli <lixueli>
2024-10-23 14:28:32 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -90,4 +90,16 @@ public class EmployeeController {
      return Result.success(pageQuery);
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    private Result startOrStop(@PathVariable Integer status,Long id){
+        employeeService.startOrStop(status,id);
+        return Result.success();
+    }
 }
