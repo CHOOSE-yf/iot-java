c01af96cc66d9c8d02ec2800f7fb9a211a5f5eb6
lixueli <lixueli>
2024-10-25 15:45:00 +0800

    菜品分页查询

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,26 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public interface DishService {
+
+
+    /**
+     * 新增菜品和口味数据
+     *
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+
+
+    /**
+     * 菜品分页查询
+     *
+     * @param dishPageQueryDTO
+     * @return
+     */
+    PageResult pegeQuery(DishPageQueryDTO dishPageQueryDTO);
+}
