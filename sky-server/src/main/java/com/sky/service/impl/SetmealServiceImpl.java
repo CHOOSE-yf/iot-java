b17341e1a0b409718c1516b6c432f159d6b10c31
lixueli <lixueli>
2024-10-28 10:44:22 +0800

    套餐起售停售

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -6,6 +6,7 @@ import com.sky.constant.MessageConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.dto.SetmealDTO;
 import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.entity.Setmeal;
 import com.sky.entity.SetmealDish;
 import com.sky.exception.DeletionNotAllowedException;
@@ -133,4 +134,30 @@ public class SetmealServiceImpl implements SetmealService {
 
 
    }
+
+    /**
+     * 套餐起售停售
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        //起售套餐时，判断套餐内是否有停售菜品，有停售菜品提示“套餐内包含未起售菜品，无法起售”
+        if (status == StatusConstant.ENABLE){
+            //select a.* from dish a left join setmeal_dish b on a.id = b.dish_id where b.setmeal_id = ?
+           List<Dish> dishList =  dishMapper.getBySetmealId(id);
+           if (dishList != null && dishList.size() > 0){
+               dishList.forEach(dish -> {
+                   if (StatusConstant.DISABLE == dish.getStatus()){
+                       throw  new StringIndexOutOfBoundsException(MessageConstant.SETMEAL_ENABLE_FAILED);
+                   }
+               });
+           }
+        }
+       Setmeal setmeal = Setmeal.builder()
+                .id(id)
+                .status(status)
+                .build();
+        setmealMapper.update(setmeal);
+    }
 }
