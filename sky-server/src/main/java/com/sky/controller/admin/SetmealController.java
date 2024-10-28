ec656fb798e13b748a9f2b289088bfe092ae8b87
lixueli <lixueli>
2024-10-28 09:43:27 +0800

    修改和查询套餐数据

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -5,6 +5,7 @@ import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.SetmealService;
+import com.sky.vo.SetmealVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +53,28 @@ public class SetmealController {
     @ApiOperation("批量删除套餐")
     public Result delete(@RequestParam List<Long> ids){
         setmealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    /**
+     *  根据id查询套餐，用于修改页面回显数据
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐，用于修改页面回显数据")
+    public Result<SetmealVO> getById( @PathVariable Long id){
+        SetmealVO setmealVO = setmealService.getByIdWithDish(id);
+        return Result.success(setmealVO);
+    }
+
+    /**
+     * 修改套餐
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改套餐")
+    public Result update(@RequestBody SetmealDTO setmealDTO){
+         setmealService.update(setmealDTO);
         return Result.success();
     }
 }
--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
+import com.sky.enumeration.OperationType;
 import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
@@ -46,6 +48,9 @@ public interface SetmealMapper {
      * 根据id删除套餐
      * @param setmealId
      */
-    @Delete("DELETE from setmeal where id = #{id}")
+    @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
+
+    @AutoFill(value = OperationType.UPDATE)
+    void update(Setmeal setmeal);
 }
